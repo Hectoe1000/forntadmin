45fa8272c0338b70fca5f7b27fa24e8e6ca01855
Hectoe1000 <https://github.com/Hectoe1000/jor.git>
2024-12-05 18:59:13 -0500

    implementando login

--- app-admin/src/app/components-base/Producto/producto-component/producto-component.component.ts
+++ app-admin/src/app/components-base/Producto/producto-component/producto-component.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { ProductoResponse } from '../models/producto-response';
 import { ProductoResponseService } from '../services/producto-response.service';
 import { CommonModule } from '@angular/common';
+import { ProductoModalComponent } from '../modals-template/producto-modal/producto-modal.component';
 
-
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-producto-component',
   standalone: true,
-  imports: [ProductoComponentComponent,CommonModule],
+  imports: [ProductoComponentComponent,CommonModule,ProductoModalComponent],
+  providers: [BsModalService] ,
   templateUrl: './producto-component.component.html',
   styleUrl: './producto-component.component.css'
 })
@@ -16,9 +18,11 @@ export class ProductoComponentComponent implements OnInit {
 
   
   producto:ProductoResponse[]=[];
+  modalRef?: BsModalRef;
 
   constructor(
-    private _productoServices:ProductoResponseService
+    private _productoServices:ProductoResponseService,
+    private modalService: BsModalService
   ){
 
   }
@@ -47,4 +51,11 @@ export class ProductoComponentComponent implements OnInit {
     });
 
 }
+
+
+openModal(template: TemplateRef<any>) {
+  this.modalRef = this.modalService.show(template);
+}
+
+
 }
--- app-admin/src/app/template/template-routing.module.ts
+++ app-admin/src/app/template/template-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TemplateGeneralComponent } from './template-general/template-general.component';
+import { LoginComponent } from '../components-base/login/componentes/login/login.component';
 
 const routes: Routes = [
 
   {
-    path:'',component:TemplateGeneralComponent,
+    path:'',component:LoginComponent
+  },
+
+  {
+
+    path:'dashboard',component:TemplateGeneralComponent,
     children:[
       {
         path:'producto',loadChildren:()=>import('../components-base/components-base.module').then(m=>m.ComponentsBaseModule)
